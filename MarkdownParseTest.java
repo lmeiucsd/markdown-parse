a0f4a7ca175ab42ca9b27c1a47127a722abbffc5
Larry Mei <88351375+lmeiucsd@users.noreply.github.com>
2022-01-26 16:39:08 -0800

    added JUnit files

---
+++ MarkdownParseTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*; // import junit's assert
+import org.junit.*; // import junit library
+
+public class MarkdownParseTest { // file's class
+    @Test // specifies that the following method is a test to JUnit
+    public void addition() { // creates method header according to JUnit syntax
+        assertEquals(2, 1 + 1); // asserts that 2 = 1+1
+    }
+}
