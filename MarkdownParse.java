675aca76db85f501b3f043a723c4addd19fa43d0
Larry Mei <88351375+lmeiucsd@users.noreply.github.com>
2022-02-02 16:42:32 -0800

    potential bug fix

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -23,7 +23,13 @@ public class MarkdownParse {
                 break;
             }
             int openParen = markdown.indexOf("(", nextCloseBracket);
+            if(openParen == -1){
+                break;
+            }
             int closeParen = markdown.indexOf(")", openParen);
+            if(closeParen == -1){
+                break;
+            }
             // System.out.println(closeParen);
             // System.out.println(markdown.length() - 1);
             // System.out.println(markdown.length());
