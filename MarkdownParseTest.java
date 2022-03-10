1756f0fb179e11fccb52969992d0e42173ceba50
Larry Mei <88351375+lmeiucsd@users.noreply.github.com>
2022-03-10 14:40:58 -0800

    Revert "testing commonmark"
    This reverts commit 0df496c46e7be128574f772fb53513735fdf740c.

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -10,87 +10,61 @@ public class MarkdownParseTest { // file's class
 
     String file1;
 
-    // @Test // specifies that the following method is a test to JUnit
-    // public void addition() { // creates method header according to JUnit syntax
-    //     assertEquals(2, 1 + 1); // asserts that 2 = 1+1
-    // }
+    @Test // specifies that the following method is a test to JUnit
+    public void addition() { // creates method header according to JUnit syntax
+        assertEquals(2, 1 + 1); // asserts that 2 = 1+1
+    }
 
     public void setup(String fileName) throws Exception {
             Path file = Path.of(fileName);
             file1 = Files.readString(file);
     }
 
-    // @Test
-    // public void customTest1() throws Exception {
-    //     // public static ArrayList<String> getLinks(String markdown) {
-    //     setup("test-file.md");
-    //     ArrayList<String> fileContent = MarkdownParse.getLinks(file1);
-    //     assertEquals(fileContent.toString(), "[https://something.com, some-page.html]");
-    // }
-
-    // @Test
-    // public void customTest2() throws Exception {
-    //     // public static ArrayList<String> getLinks(String markdown) {
-    //     setup("new-file.md");
-    //     ArrayList<String> fileContent = MarkdownParse.getLinks(file1);
-    //     assertEquals(fileContent.toString(), "[https://something.com, some-page.html]");
-    // }
-
-    // @Test
-    // public void customTest3() throws Exception {
-    //     // public static ArrayList<String> getLinks(String markdown) {
-    //     setup("new-file2.md");
-    //     ArrayList<String> fileContent = MarkdownParse.getLinks(file1);
-    //     assertEquals(fileContent.toString(), "[google.com]");
-    // }
-
-    // @Test
-    // public void customTest4() throws Exception {
-    //     // public static ArrayList<String> getLinks(String markdown) {
-    //     setup("new-file3.md");
-    //     ArrayList<String> fileContent = MarkdownParse.getLinks(file1);
-    //     assertEquals(fileContent.toString(), "[# title\n\n[]link goes here]");
-    // }
-
-    // @Test
-    // public void customTest5() throws Exception {
-    //     // public static ArrayList<String> getLinks(String markdown) {
-    //     setup("test-file2.md");
-    //     ArrayList<String> fileContent = MarkdownParse.getLinks(file1);
-    //     assertEquals(fileContent.toString(), "[https://something.com, some-page.html]");
-    // }
-
-    // @Test
-    // public void customTest6() throws Exception {
-    //     // public static ArrayList<String> getLinks(String markdown) {
-    //     setup("test-file3.md");
-    //     ArrayList<String> fileContent = MarkdownParse.getLinks(file1);
-    //     assertEquals(fileContent.toString(), "[]");
-    // }
-    //     @Test
-    // public void snippetTest1() throws Exception {
-    //     // public static ArrayList<String> getLinks(String markdown) {
-    //     setup("snippet1.md");
-    //     ArrayList<String> fileContent = MarkdownParse.getLinks(file1);
-    //     System.out.println(fileContent.toString());
-    //     assertEquals(fileContent.toString(), "[`google.com, google.com, ucsd.edu]");
-    // }
-
-    //         @Test
-    // public void snippetTest2() throws Exception {
-    //     // public static ArrayList<String> getLinks(String markdown) {
-    //     setup("snippet2.md");
-    //     ArrayList<String> fileContent = MarkdownParse.getLinks(file1);
-    //     System.out.println(fileContent.toString());
-    //     assertEquals(fileContent.toString(), "[a.com, a.com(()), example.com");
-    // }
-
+    @Test
+    public void customTest1() throws Exception {
+        // public static ArrayList<String> getLinks(String markdown) {
+        setup("test-file.md");
+        ArrayList<String> fileContent = MarkdownParse.getLinks(file1);
+        assertEquals(fileContent.toString(), "[https://something.com, some-page.html]");
+    }
 
     @Test
-    public void snippetTest3() throws Exception {
+    public void customTest2() throws Exception {
         // public static ArrayList<String> getLinks(String markdown) {
-        setup("snippet3.md");
+        setup("new-file.md");
         ArrayList<String> fileContent = MarkdownParse.getLinks(file1);
-        assertEquals(fileContent.toString(), "[https://ucsd-cse15l-w22.github.io/]");
+        assertEquals(fileContent.toString(), "[https://something.com, some-page.html]");
+    }
+
+    @Test
+    public void customTest3() throws Exception {
+        // public static ArrayList<String> getLinks(String markdown) {
+        setup("new-file2.md");
+        ArrayList<String> fileContent = MarkdownParse.getLinks(file1);
+        assertEquals(fileContent.toString(), "[google.com]");
+    }
+
+    @Test
+    public void customTest4() throws Exception {
+        // public static ArrayList<String> getLinks(String markdown) {
+        setup("new-file3.md");
+        ArrayList<String> fileContent = MarkdownParse.getLinks(file1);
+        assertEquals(fileContent.toString(), "[# title\n\n[]link goes here]");
+    }
+
+    @Test
+    public void customTest5() throws Exception {
+        // public static ArrayList<String> getLinks(String markdown) {
+        setup("test-file2.md");
+        ArrayList<String> fileContent = MarkdownParse.getLinks(file1);
+        assertEquals(fileContent.toString(), "[https://something.com, some-page.html]");
+    }
+
+    @Test
+    public void customTest6() throws Exception {
+        // public static ArrayList<String> getLinks(String markdown) {
+        setup("test-file3.md");
+        ArrayList<String> fileContent = MarkdownParse.getLinks(file1);
+        assertEquals(fileContent.toString(), "[]");
     }
     }
